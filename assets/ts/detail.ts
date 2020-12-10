72ce6a09e4029acbee66bacb207813ca9d84ae4f
Jakob <26389546+Jakob-em@users.noreply.github.com>
2020-12-10 14:56:35 +0100

    Fix multiline label

--- assets/ts/detail.ts
+++ assets/ts/detail.ts
@@ -1,11 +1,11 @@
 import * as d3 from 'd3';
-import { byAge, byCitySize, PercentagePoint } from './data';
+import {byAge, byCitySize, PercentagePoint} from './data';
 
 
 let margins = {top: 10, right: 10, bottom: 10, left: 10};
 let detailDimensions = {
-  width: 245 - margins.left - margins.right,
-  height: 100 - margins.top - margins.bottom
+    width: 245 - margins.left - margins.right,
+    height: 100 - margins.top - margins.bottom
 }
 
 
@@ -20,48 +20,66 @@ let detailChart = d3.select('#detail-chart')
 const perColumn = 5
 
 function showByAge() {
-  detailChart.selectAll(".data-point")
-      .data(createMappedData(byAge))
-      .transition()
-      .attr("class", d => d + ' data-point')
+    detailChart.selectAll(".data-point")
+        .data(createMappedData(byAge))
+        .transition()
+        .attr("class", d => d + ' data-point')
 
-  detailChart.selectAll(".label")
-      .data(buildLabels(byAge))
-      .transition()
-      .text(d => d.label)
-      .attr('x', (d) => d.posX)
-
+    detailChart.selectAll(".label")
+        .data(buildLabels(byAge))
+        .transition()
+        .attr('x', (d) => d.posX)
+        .each(addLines)
 }
 
 function showByPopulation() {
-  detailChart.selectAll(".data-point")
-      .data(createMappedData(byCitySize))
-      .transition()
-      .attr("class", d => d + ' data-point')
+    detailChart.selectAll(".data-point")
+        .data(createMappedData(byCitySize))
+        .transition()
+        .attr("class", d => d + ' data-point')
 
-  detailChart.selectAll(".label")
-      .data(buildLabels(byCitySize))
-      .transition()
-      .text(d => d.label)
-      .attr('x', (d) => d.posX)
+    detailChart.selectAll(".label")
+        .data(buildLabels(byCitySize))
+        .transition()
+        .attr('x', (d) => d.posX)
+        .each(addLines)
+}
+
+function addLines(d) {
+    let lines = d3.select(this).selectAll('tspan')
+        .data(d.label.split("\n").map((d, i) => ({label: d, index: i})))
+
+    let fillLabelDetails = function () {
+        d3.select(this).transition()
+            .attr('dy', (l) => l.index == 0 ? "0em" : "1em")
+            .text((l) => l.label)
+            .attr('x', d.posX)
+    }
+
+    lines.enter()
+        .append("tspan")
+        .attr("text-anchor", "middle")
+        .each(fillLabelDetails)
+    lines.exit().remove()
+    lines.each(fillLabelDetails)
 }
 
 
 function createMappedData(data: PercentagePoint[]) {
-  return data.map(value => Array.apply(null, Array(value.percentage)).map(() => value.class))
-      .reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
+    return data.map(value => Array.apply(null, Array(value.percentage)).map(() => value.class))
+        .reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
 }
 
 function buildLabels(data: PercentagePoint[]) {
-  let sum = 0
-  return data.map(value => {
-    let center = sum + value.percentage / 2
-    sum += value.percentage;
-    return {
-      label: value.label,
-      posX: (center / 100) * 20 * 12 - 6
-    }
-  });
+    let sum = 0
+    return data.map(value => {
+        let center = sum + value.percentage / 2
+        sum += value.percentage;
+        return {
+            label: value.label,
+            posX: (center / 100) * 20 * 12 - 6
+        }
+    });
 }
 
 detailChart.selectAll(".data-point")
@@ -79,9 +97,7 @@ detailChart.selectAll(".label")
     .enter()
     .append("text")
     .classed("label", true)
-    .text(d => d.label)
     .attr("text-anchor", "middle")
-    .attr('x', (d) => d.posX)
     .attr('y', perColumn * 12 + 5)
 
 showByAge()
@@ -89,10 +105,10 @@ showByAge()
 let isAge = true
 
 document.getElementById('detail-chart').onclick = () => {
-  if (isAge) {
-    showByPopulation()
-  } else {
-    showByAge()
-  }
-  isAge = !isAge
+    if (isAge) {
+        showByPopulation()
+    } else {
+        showByAge()
+    }
+    isAge = !isAge
 }
