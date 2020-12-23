8cd92c985f8cca4ce6c5c35b62ab164b1280c09f
Jakob-em <26389546+Jakob-em@users.noreply.github.com>
2020-12-23 19:41:41 +0100

    Add texts and more effects to detail view

--- assets/ts/detail.ts
+++ assets/ts/detail.ts
@@ -4,7 +4,7 @@ import {byAge, byCitySize, PercentagePoint} from './data';
 
 let margins = {top: 0, right: 10, bottom: 0, left: 0};
 let detailDimensions = {
-  width: 300 - margins.left - margins.right,
+  width: 350 - margins.left - margins.right,
   height: 180 - margins.top - margins.bottom
 }
 
@@ -23,8 +23,7 @@ let detailChart = d3.select('#detail-chart')
     .attr('preserveAspectRatio', 'xMinYMin meet')
     .attr('viewBox', `0 0 ${detailDimensions.width + margins.right + margins.left} ${detailDimensions.height + margins.top + margins.bottom}`)
     .append('g')
-    .attr('transform',
-        'translate(' + margins.left + ',' + margins.top + ')')
+    .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')')
 
 
 const perColumn = 5
@@ -54,19 +53,70 @@ function showByAge() {
       .transition()
       .attr('fill-opacity', 1)
       .attr('transform', horizontalPositionAndScale)
-      .attr('class', d => d.class + ' data-point')
+      .each(function (d) {
+        this.classList.remove('mid', 'low', 'high')
+        this.classList.add(d.class);
+      })
 
   detailChart.selectAll('.label')
       .data(buildLabels(byAge, 'horizontal'))
       .transition()
       .attr('x', (d) => d.posX)
-      .attr('y', (perColumn + 0.5) * spacingVertical)
+      .attr('y', (perColumn + 0.5) * spacingVertical + 20)
       .attr('class', d => d.class + ' label')
-      .each(addLines)
+      .each(addLinesFunction('middle'))
+
+  // Highlight biggest group
+  detailChart.selectAll('.data-point').classed('special-low-gradient', (d, i) => i < 10)
+
+  const highlightLine = d3.path();
+  highlightLine.moveTo(12, 15)
+  highlightLine.lineTo(18, 5)
+  highlightLine.lineTo(50, 5)
+
+  const specialPath = detailChart.append('path')
+      .classed('special-line', true)
+      .attr('d', highlightLine)
+  fadeInDelayed(specialPath)
+
+  const specialText = detailChart.append('text')
+      .classed('special label', true)
+      .attr('x', 55)
+      .attr('y', 7)
+      .text('25-30')
+  fadeInDelayed(specialText)
+
+  addInfoText(0, (perColumn) * spacingVertical + 50,
+      `2019 waren knapp <tspan class="low bold">27 Prozent</tspan> der Alleinlebenden den <tspan class="low bold">unter 35 jährigen</tspan> 
+zuzuschreiben, während <tspan class="high bold">40 Prozent</tspan> bereits das <tspan class="high bold">60. Lebensjahr vollendet</tspan> hatten.
+ 
+Die größte Gruppe unter den Singlehaushalten bilden dabei die
+<tspan class="special bold">25- bis unter 30-jährigen </tspan>, ihr Anteil beträgt <tspan class="special bold">rund ein Zehntel</tspan>.
+      `)
+}
+
+function addInfoText(x, y, content) {
+  detailChart.select('.info').remove()
+  const infoText = detailChart
+      .append('text')
+      .classed('info', true)
+      .attr('y', y)
+      .datum({
+        label: content, posX: x
+      }).each(addLinesFunction('start', '1.2em'))
+
+  fadeInDelayed(infoText)
+}
+
+function fadeInDelayed(e) {
+  e.attr('opacity', 0)
+      .transition()
+      .delay(200)
+      .attr('opacity', 1)
 }
 
 function horizontalPositionAndScale(d, i) {
-  return `translate(${(Math.floor(i / perColumn) * spacingHorizontal)}, ${((i % perColumn) * spacingVertical)}) scale(0.04, 0.042)`
+  return `translate(${(Math.floor(i / perColumn) * spacingHorizontal)}, ${((i % perColumn) * spacingVertical) + 18}) scale(0.04, 0.042)`
 }
 
 function barPositionAndScale(d) {
@@ -75,6 +125,9 @@ function barPositionAndScale(d) {
 
 function showByPopulation() {
   toggleDetailButtons(false)
+
+  detailChart.select('.info').remove()
+  detailChart.select('.special-line').remove()
 
   detailChart.selectAll('.data-point')
       .data(createMappedData(byCitySize))
@@ -98,29 +151,47 @@ function showByPopulation() {
       .attr('class', d => d.class + ' label')
       .attr('x', (d) => d.posX)
       .attr('y', 175)
-      .each(addLines)
+      .each(addLinesFunction('middle'))
 
   detailChart.selectAll('.label')
       .data(buildLabels(byCitySize, 'vertical')).exit().remove()
+
+  addInfoText(160, 20,
+      `In den bayerischen Großstädten mit
+<tspan class="high bold">mehr als 100 000 Einwohnern</tspan> fiel der
+Anteil der Einpersonenhaushalte
+besonders hoch aus. Hier bestand mehr
+als jeder zweite Haushalt (<tspan class="high bold">53 Prozent</tspan>) aus
+nur einer Person.
+ 
+Im Vergleich dazu waren in Gemeinden
+mit <tspan class="mid bold">weniger als 5 000 Einwohnern</tspan>
+Singlehaushalte mit einem Anteil von
+rund <tspan class="mid bold">34 Prozent</tspan> weit weniger häufig vertreten
+        `)
 }
 
-function addLines(d) {
-  let lines = d3.select(this).selectAll('tspan')
-      .data(d.label.split('\n').map((d, i) => ({label: d, index: i})))
+function addLinesFunction(textAnchor, lineSpacing = '1em') {
 
-  let fillLabelDetails = function () {
-    d3.select(this).transition()
-        .attr('dy', (l) => l.index == 0 ? '0em' : '1em')
-        .text((l) => l.label)
-        .attr('x', d.posX)
+  return function addLines(d) {
+    let lines = d3.select(this).selectAll('tspan')
+        .data(d.label.split('\n').map((d, i) => ({label: d, index: i})))
+
+    let fillLabelDetails = function () {
+      d3.select(this)
+          .html((l) => l.label)
+          .transition()
+          .attr('dy', (l) => l.index == 0 ? '0em' : lineSpacing)
+          .attr('x', d.posX)
+    }
+
+    lines.enter()
+        .append('tspan')
+        .attr('text-anchor', textAnchor)
+        .each(fillLabelDetails)
+    lines.exit().remove()
+    lines.each(fillLabelDetails)
   }
-
-  lines.enter()
-      .append('tspan')
-      .attr('text-anchor', 'middle')
-      .each(fillLabelDetails)
-  lines.exit().remove()
-  lines.each(fillLabelDetails)
 }
 
 
@@ -154,6 +225,7 @@ function buildLabels(data: PercentagePoint[], direction: 'horizontal' | 'vertica
 }
 
 showByAge()
+
 
 function toggleDetailButtons(isByAge: boolean) {
   d3.select('#by-population-button').classed('active', !isByAge)
